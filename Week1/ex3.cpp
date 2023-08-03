3b7906f15fcccc8fbcbb1af4851825468a4e4acb
Arthur <crfernandesarthur@gmail.com>
2023-08-03 14:04:04 -0300

    Comentários no cod
    Comentários explicativos em Ex03 e Ex04.

--- Week1/ex3.cpp
+++ Week1/ex3.cpp
@@ -1,35 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 123;
+const int MAX = 123; // Número máximo de linhas e colunas que a matriz pode ter
 
-int columns, rows;
-string arr[MAX];
-bool vis[MAX][MAX] = {false};
-bool ans = false;
-int direction;
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
-//      ^ v < >
+int columns, rows; // número de colunas e linhas
+string arr[MAX]; // matriz
+bool vis[MAX][MAX] = {false}; // matriz para verificar posições que já foram visitadas
+bool ans = false; // variável para dizer se encontramos o caracter '*'
+
+// arrays que auxiliam no percorrimento da matriz (DFS)
+int dx[] = {-1,1,0,0}; // se refere a linha da matriz
+int dy[] = {0,0,-1,1}; // se refere a coluna da matriz
+
+int direction; // guarda o índice dos arrays dx e dy (saber qual direção está indo)
+
+/* ==========================================================================================
+|    EXEMPLO: Estamos na posição (2,3) da matriz
+|    se direction for 0 e queremos se movimentar nessa direção, então a nova posição será:
+|    (2+dx[0], 3+dy[0]) = (2+(-1), 3+0) = (1,3)
+|    Ou seja, de (2,3) fomos para (1,3). Fomos uma posição para cima
+|    Segue o mesmo raciocínio para os demais índices.
+|    direction = 0 => movimento para cima
+|    direction = 1 => movimento para baixo
+|    direction = 2 => movimento para esquerda
+|    direction = 3 => movimento para direita  
+|  ========================================================================================== */ 
 
 void dfs(int x, int y) {
-    if(arr[x][y] == '*') {
+    if(arr[x][y] == '*') { // fim do problema, sucesso!
         ans = true;
         return;
-    } 
+    }
+    // mudança de direção
     else if(arr[x][y] == '^') direction = 0;
     else if(arr[x][y] == 'v') direction = 1;
     else if(arr[x][y] == '<') direction = 2;
     else if(arr[x][y] == '>') direction = 3;
     
+    // calculando nova posição
     int nx = x+dx[direction];
     int ny = y+dy[direction];
+    
+    // verificação se é uma posição válida ou então se já foi visitado
     if(nx < 0 or nx >= rows or ny < 0 or ny >= columns or vis[nx][ny]) return;
+    
     vis[nx][ny] = true;
     dfs(nx,ny);
 }
 
 void solve() {
+    // Entrada de dados
     cin >> columns >> rows;
     for(int i = 0; i < rows; i++) {
         cin >> arr[i];
@@ -38,6 +58,7 @@ void solve() {
     vis[0][0] = true;
     dfs(0,0);
     
+    // Saída
     if(ans) cout << "*\n";
     else cout << "!\n";
 }
--- Week1/ex4.cpp
+++ Week1/ex4.cpp
@@ -1,45 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 123;
+const int MAX = 123; // número maximo de linhas e colunas que pode conter a matriz
 
-string arr[MAX];
-bool vis[MAX][MAX];
+string arr[MAX]; // matriz
+bool vis[MAX][MAX]; // matriz para indicar as posições ja visitadas
 int ans, rows, columns;
-int dx[] = {-1,-1,-1,1, 1,1, 0,0};
-int dy[] = { 0,-1, 1,0,-1,1,-1,1};
-// cima, dig. cima esq, dig. cima dir, bx, dig. bx esq, dig. bx dir, esq, dir
 
-void dfs(int x, int y) {
+// arrays que auxiliam no percorrimento da matriz (DFS)
+int dx[] = {-1,-1,-1,1, 1,1, 0,0}; // se refere a linha da matriz
+int dy[] = { 0,-1, 1,0,-1,1,-1,1}; // se refere a coluna da matriz
+
+/* ==========================================================================================
+|    EXEMPLO: Estamos na posição (2,3) da matriz
+|    se índice de dx e dy for 0 e queremos se movimentar, então a nova posição será:
+|    (2+dx[0], 3+dy[0]) = (2+(-1), 3+0) = (1,3)
+|    Ou seja, de (2,3) fomos para (1,3). Fomos uma posição para cima
+|    Segue o mesmo raciocínio para os demais índices.
+|    índice = 0 => movimento para cima
+|    índice = 1 => movimento para diagonal superior esquerda
+|    índice = 2 => movimento para diagonal superior direita
+|    índice = 3 => movimento para baixo
+|    índice = 4 => movimento para diagonal inferior esquerda
+|    índice = 5 => movimento para diagonal inferior direita
+|    índice = 6 => movimento para esquerda
+|    índice = 7 => movimento para direita  
+|  ========================================================================================== */ 
+
+void dfs(int x, int y) { // x e y é a posição em que irei começar percorrer a matriz através da DFS
+    
+    // Procurando todas as posições que contem '@' e são adjacentes a arr[x][y]
+    // (elementos da nova componente conexa)
     for(int i = 0; i < 8; i++) {
+
+        // calculando nova posição
         int nx = x+dx[i];
         int ny = y+dy[i];
+        
+        // verificação se é uma posição válida ou é diferente de '@' ou então se já foi visitado
         if(nx < 0 or nx >= rows or ny < 0 or ny >= columns or arr[nx][ny] != '@' or vis[nx][ny]) continue;
+        
         vis[nx][ny] = true;
         dfs(nx,ny);
     }
 }
 
 void solve() {
-    while(cin >> rows >> columns) {
-        if(rows == 0) return;
+    while(cin >> rows >> columns) { // Leitura dos dados de entrada (numero de linhas e coluna da matriz)
+        if(rows == 0) return; // termina as entradas e encerra o problema
 
-        memset(vis,false,sizeof(vis));
+        memset(vis,false,sizeof(vis)); // seto todas as posições da matriz 
         ans = 0;
 
+        // Leitura da matriz de entrada
         for(int i = 0; i < rows; i++) {
             cin >> arr[i];
         }
 
+        // percorrendo a matriz
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
-                if(arr[i][j] == '@' and !vis[i][j]) {
-                    ans++;
+                if(arr[i][j] == '@' and !vis[i][j]) { // encontrei uma nova componente conexa
+                    ans++; // +1 componente
                     vis[i][j] = true;
-                    dfs(i,j);
+                    dfs(i,j); // marco todas as posições que fazem parte dessa componente
                 }
             }
         }
+        // Saída
         cout << ans << "\n";
     }
     
