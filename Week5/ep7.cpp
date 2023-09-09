6699f8c18d570adc48b59837495e23802f0e73fd
Arthur <crfernandesarthur@gmail.com>
2023-09-09 10:39:37 -0300

    feat: ok

--- Week5/ep7.cpp
+++ Week5/ep7.cpp
@@ -12,8 +12,10 @@ public:
     int v1,v2;
     lli cost;
 
+    // Construtor
     Edge(int v1, int v2, lli cost) : v1(v1), v2(v2), cost(cost) {}
 
+    // Método que será usado para ordenar o vector
     bool operator < (const Edge& other) {
         return (cost < other.cost);
     }
@@ -23,11 +25,13 @@ int parent[MAX];
 int weight[MAX];
 vector<Edge> ans;
 
+// Procurar/atualizar o pai de um vertice
 int find(int idx) {
     if(parent[idx] == idx) return idx;
     return (parent[idx] = find(parent[idx]));
 }
 
+// Unir duas componentes
 void Union(Edge e) {
     int v1 = e.v1;
     int v2 = e.v2;
@@ -36,7 +40,9 @@ void Union(Edge e) {
     int parentV1 = find(v1);
     int parentV2 = find(v2);
 
+    // Se os vertices nao tem mesmo pai, ou seja, nao fazem parte da mesma componente, entao vamos uni-los
     if(parentV1 != parentV2) {
+        // verificacoes para equilibrar a arvore
         if(weight[v1] < weight[v2]) parent[parentV1] = parentV2;
         else if(weight[v1] > weight[v2]) parent[parentV2] = parentV1;
         else {
@@ -53,6 +59,7 @@ void solve() {
     
     vector<Edge> edges;
 
+    // Leitura do input
     for(int i = 0; i < n; i++) {
         parent[i] = i;
         weight[i] = 0;
@@ -65,12 +72,15 @@ void solve() {
         }
     }
 
+    // Ordenando o vetor
     sort(edges.begin(),edges.end());
 
+    // Gerando a MST
     for(auto e : edges) {
         Union(e);
     }
 
+    // Output
     for(auto e : ans) {
         cout << e.v1+1 << " " << e.v2+1 << "\n";
     }
